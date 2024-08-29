454021ba27ca3da43628b3e4c218c7cd78f75fde
Tej Birring <tejbirring@gmail.com>
2024-08-29 02:05:18 +0100

    Improve professional experience frontmatter model, fix/adjust component to render 'contract type'.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,10 +1,26 @@
 import { defineCollection, z } from "astro:content";
 
+const ContractType =    z.literal("Permanent")          // permanent full-time or part-time staff member
+                        .or(z.literal("Contractor"))    // contractor or freelancer
+                        .or(z.literal("Founder"))       // founder or co-founder
+                        ;
+
+// const Location = z.union([
+//     z.object({
+//         type: z.literal("Remote")
+//     }),
+//     z.object({
+//         type: z.literal("On-Site"),
+//         location: z.string(),
+//     })
+// ]);
+
 const professionalExperienceCollection = defineCollection({
     type: "content",
     schema: z.object({
         title: z.string(),
-        company: z.string(),
+        company: z.string().nullish(),
+        "contract type": ContractType,
         location: z.string(),
         from: z.object({
             month: z.number().int().min(1).max(12),
@@ -13,12 +29,11 @@ const professionalExperienceCollection = defineCollection({
         to: z.object({
             month: z.number().int().min(1).max(12),
             year: z.number()
-        }).or(z.literal("present")),
+        }).or(z.literal("Present")),
         skills: z.array(z.string()).nullish()
     })
 });
 
 export const collections = {
     "professional-experience": professionalExperienceCollection,
-
 }
